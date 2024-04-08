2a2b982ee0a228f23583c4ed98d7aa5e6696f44b
eduvoltatone <108620108+eduvoltatone@users.noreply.github.com>
2024-04-08 19:47:55 -0300

    Deletei tudo

--- Exercicio 3/Controller/dicionarioController.ts
+++
@@ -1,5 +0,0 @@
-import { construirDicionario, ArvoreBuscaBinaria } from '../Model/arvoreBuscaBinaria';
-
-export function buscarPalavra(chave: string, dicionario: ArvoreBuscaBinaria): boolean {
-    return dicionario.buscar(chave);
-}
